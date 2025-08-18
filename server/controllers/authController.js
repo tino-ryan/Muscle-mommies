85ff37c45dd87775ccd2d8367cfa7c3edc866046
tino-ryan <tinogozho2@gmail.com>
2025-08-19 00:45:26 +0200

    rename User.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,6 @@
 const admin = require('../config/firebase'); // Firebase Admin SDK
 
-const User = require('../models/user'); // User model
+const User = require('../models/User'); // match the file
 
 // Signup via email & password
 exports.signup = async (req, res) => {
