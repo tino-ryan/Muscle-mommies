2709fe93c27842c00fc3ba94c25908b911c7d675
aimeelayla <163777399+aimeelayla@users.noreply.github.com>
2025-09-29 21:50:24 +0200

    Delete client/src/utils.js

--- client/src/utils.js
+++
@@ -1,9 +0,0 @@
-// src/utils.js
-export const add = (a, b) => a + b;
-export const multiply = (a, b) => a * b;
-export const greetUser = (name) => {
-  if (!name) {
-    throw new Error('Name is required');
-  }
-  return `Hello, ${name}!`;
-};
